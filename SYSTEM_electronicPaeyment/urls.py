21c0b49817d4301b332ffb907ee9ce3d0f4aa703
GHAZAL <GHAZAL@exampel.com>
2025-09-03 03:39:23 -0700

    إصلاح errors في urls.py

--- SYSTEM_electronicPaeyment/urls.py
+++ SYSTEM_electronicPaeyment/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for systemElectronicPaeyment project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
@@ -15,18 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/',include('APPLI.urls')),
-
-
+    path('app/', include('APPLI.urls')),
+    path('', RedirectView.as_view(url='/admin/')),  
 ]
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,                    document_root=settings.MEDIA_ROOT)
 
-
-
+# servir archivos estáticos en desarrollo y producción
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
